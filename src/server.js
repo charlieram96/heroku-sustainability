d620fefc100439e3b2f9d5439399bc4798454417
dylan-mdg <dylan@maxwelldesigngroup.com>
2023-10-27 10:13:44 -0400

    updates

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+require('dotenv').config(); 
+const client = require('smartsheet');
 const path = require('path');
 
 const app = express();
@@ -15,5 +17,33 @@ app.listen(PORT, HOST, () => {
   console.log(`Server is running on http://${HOST}:${PORT}`);
 });
 
+console.log(process.env.SMARTSHEET_ACCESS_TOKEN)
 
+const smartsheet = client.createClient({
+  accessToken: process.env.SMARTSHEET_ACCESS_TOKEN, 
+  logLevel: 'info'
+});
 
+var options = {
+  queryParameters: {
+    include: "attachments",
+    includeAll: true
+  }
+};
+
+smartsheet.sheets.listSheets(options)
+  .then(function (result) {
+    var sheetId = result.data[0].id;  // Choose the first sheet
+
+    // Load one sheet
+    smartsheet.sheets.getSheet({id: sheetId})
+      .then(function(sheetInfo) {
+        console.log(sheetInfo);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  })
+  .catch(function(error) {
+    console.log(error);
+  });
