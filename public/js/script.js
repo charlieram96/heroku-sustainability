c2ca68bf5a85899e0ebb96baa5a9b373c218e41b
dylan-mdg <dylan@maxwelldesigngroup.com>
2023-10-31 23:46:23 -0400

    update

--- public/js/script.js
+++ public/js/script.js
@@ -3,7 +3,6 @@ $(document).ready(function() {
 
   $.getJSON('/src/data/sheetInfo.json', function(data) {
     allData = data;
-    console.log(allData.rows);
   });
 });
 
@@ -55,51 +54,119 @@ const embed = {
 
 
 }
-document.getElementById("recycleClick").addEventListener("click", showRecycling);
-document.getElementById("wasteClick").addEventListener("click", showWaste);
+
+var subCats = document.getElementById('subCategories');
+var activeButton = document.getElementsByClassName('sub-cat');
+var actBut = document.getElementById('actBut');
+let type = 'Recycling';
+
+for(var i = 0; i < activeButton.length; i++) {
+  activeButton[i].addEventListener('click', function() {
+    var current = document.getElementsByClassName('active');
+    current[0].className = current[0].className.replace(' active', '');
+    this.className += ' active';
+    let vlue = this.innerHTML;
+    console.log(vlue);
+    type = vlue;
+  });
+}
+
+actBut.addEventListener('click', function() {
+  if($('#actBut').hasClass('active')) {
+    showRecycling();
+  } else {
+    document.getElementById('optionCards').style.display = 'none';
+  }
+});
+
+if($('#actBut').hasClass('active')) {
+  showRecycling();
+}
 
 function showRecycling() {
   var optionCards = document.getElementById('optionCards');
-  var features = embed.features[0].properties;
-  features.forEach(function(feature) {
-    optionCards.innerHTML +=
-    '<div class="col mb-4">' +
-      '<div class="card card-bg p-3">' +
-        '<div class="card-body">' +
-          '<h5 class="card-title text-light">' +
-           feature.subCategory +
-          '</h5>' +
-          '<p class="card-text text-light"><small>' +
-            feature.description +
-          '</small></p>' +
-          '<div class="d-flex gap-4 justify-content-start">' +
-          '<button class="btn btn-light btn-sm second-active rounded-pill px-3" type="button">Learn More</button>' +
-          '<button class="btn btn-light btn-sm rounded-pill px-3" type="button"> Select </button>' +
-          '</div></div></div></div>'
-    });
+  // document.getElementById('optionCards').style.display = 'none';
+  if (type === 'Recycling') {
+    var features = embed.features[0].properties;
+    features.forEach(function(feature) {
+      optionCards.innerHTML +=
+      '<div class="col mb-4">' +
+        '<div class="card card-bg p-3">' +
+          '<div class="card-body">' +
+            '<h5 class="card-title text-light">' +
+             feature.subCategory +
+            '</h5>' +
+            '<p class="card-text text-light"><small>' +
+              feature.description +
+            '</small></p>' +
+            '<div class="d-flex gap-4 justify-content-start">' +
+            '<button class="btn btn-light btn-sm second-active rounded-pill px-3" type="button">Learn More</button>' +
+            '<button class="btn btn-light btn-sm rounded-pill px-3" type="button"> Select </button>' +
+            '</div></div></div></div>'
+      });
+  } else if (type == 'Waste Management') {
+    var features = embed.features[1].properties;
+    features.forEach(function(feature) {
+      optionCards.innerHTML +=
+      '<div class="col mb-4">' +
+        '<div class="card card-bg p-3">' +
+          '<div class="card-body">' +
+            '<h5 class="card-title text-light">' +
+             feature.subCategory +
+            '</h5>' +
+            '<p class="card-text text-light"><small>' +
+              feature.description +
+            '</small></p>' +
+            '<div class="d-flex gap-4 justify-content-start">' +
+            '<button class="btn btn-light btn-sm second-active rounded-pill px-3" type="button">Learn More</button>' +
+            '<button class="btn btn-light btn-sm rounded-pill px-3" type="button"> Select </button>' +
+            '</div></div></div></div>'
+      });
+  } else {
+    console.log(type);
+  }
+  
   
 }
 
-function showWaste() {
-  var optionCards = document.getElementById('optionCards');
-  var properties = embed.features.properties;
-  properties.forEach(function(property) {
-    optionCards.innerHTML =
-    `<div class="col mb-4">
-      <div class="card card-bg p-3">
-        <div class="card-body">
-          <h5 class="card-title text-light">
-            this is a test
-          </h5>
-          <p class="card-text text-light"><small>
-            nodescription
-          </small></p>
-          <div class="d-flex gap-4 justify-content-start">
-          <button class="btn btn-light btn-sm second-active rounded-pill px-3" type="button">Learn More</button>
-          <button class="btn btn-light btn-sm rounded-pill px-3" type="button"> Select </button>
-          </div>
-        </div>
-      </div>
-    </div>`
-  });
-}
+var groupColumn = 0;
+var table = $('#example').DataTable({
+    columnDefs: [{ visible: false, targets: groupColumn }],
+    order: [[groupColumn, 'asc']],
+    paging: false,
+    displayLength: 25,
+    info: false,
+    drawCallback: function (settings) {
+        var api = this.api();
+        var rows = api.rows({ page: 'current' }).nodes();
+        var last = null;
+ 
+        api.column(groupColumn, { page: 'current' })
+            .data()
+            .each(function (group, i) {
+                if (last !== group) {
+                    $(rows)
+                        .eq(i)
+                        .before(
+                            '<tr class="group"><td colspan="5">' +
+                                group +
+                                '</td></tr>'
+                        );
+ 
+                    last = group;
+                }
+            });
+    }
+});
+ 
+// Order by the grouping
+$('#example tbody').on('click', 'tr.group', function () {
+    var currentOrder = table.order()[0];
+    if (currentOrder[0] === groupColumn && currentOrder[1] === 'asc') {
+        table.order([groupColumn, 'desc']).draw();
+    }
+    else {
+        table.order([groupColumn, 'asc']).draw();
+    }
+});
+
