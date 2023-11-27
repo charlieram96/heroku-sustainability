1d58957c7d41635231dd97223f916f492abea2c5
dylan-mdg <dylan@maxwelldesigngroup.com>
2023-11-27 03:01:21 -0500

    updates

--- public/js/builder.js
+++ public/js/builder.js
@@ -18,34 +18,48 @@ fetch('/src/data/final-data.json')
 /* Create Category Buttons */
 function createCategories() {
   subCategories.features.forEach(function(feature) {
-    let categoryButtons = document.getElementById('category-buttons');
-    categoryButtons.innerHTML += `<button style="font-size: 13px;" onclick="catPos(this)" class="category-button rounded-0 py-3 btn btn-primary" type="button">${feature.category}</button>`
+    let categoryButtons = document.getElementById('categoryButtons');
+    categoryButtons.classList.add('btn-group', 'd-flex', 'flex-grow-1')
+    categoryButtons.innerHTML += 
+    `<button style="font-size: 14px;" onclick="getCategoryPosition(this)" class="category-button rounded-0 py-3 btn btn-primary" type="button">${feature.category}</button>`
   });
-  let categoryButton = document.getElementsByClassName('category-button');
-  categoryButton[0].className += ' act-cat';
+  let activeCategory = document.getElementsByClassName('category-button');
+  activeCategory[0].className += ' active-category';
 }
 
 /* Create Category Description */
+function showCategoryDescription(categoryIndex) {
+  var categoryDescription = document.getElementById('categoryDescription');
+  categoryDescription.innerHTML = '';
+  var feature = subCategories.features[categoryIndex];
+  categoryDescription.innerHTML += 
+  `<h6 class="text-light">${feature.subheader}</h6>
+  <p class="pt-2 mb-0 text-light fs-6">${feature.description}</p>`
+}
 
 /* Change Category Active Class and show Sub Categories */
 function createSubCategories(categoryIndex) {
   let subCategoryButtons = document.getElementById('subCategoryButtons');
+  subCategoryButtons.classList.add('overflow-auto', 'd-flex', 'gap-4', 'justify-content-start', 'py-3')
   subCategoryButtons.innerHTML = '';
   let subCategory = subCategories.features[categoryIndex].properties
   subCategory.forEach(function(subCategory) {
-    subCategoryButtons.innerHTML += '<button class="text-nowrap sub-category btn btn-light rounded-pill px-3" type="button" onclick="subPos(this)">' + subCategory.subCategory + '</button>'
+    subCategoryButtons.innerHTML += 
+    `<button class="text-nowrap sub-category btn btn-light rounded-pill px-3" type="button" onclick="getSubcategoryPosition(this)"> ${subCategory.subCategory}</button>`
   });
   let subCategoryClass = document.getElementsByClassName('sub-category');
-  let subCategoryVal = subCategoryClass[0]
-  subCategoryVal.className += ' active';
+  let subCategoryPosition = subCategoryClass[0]
+  subCategoryPosition.className += ' active';
 }
 
-function catPos(el) {
+/* Get the position of the current active Category */ 
+function getCategoryPosition(el) {
+  typeVal = 0;
   var optionCards = document.getElementById('optionCards');
   optionCards.innerHTML = '';
-  let currCat = document.getElementsByClassName('act-cat');
-  currCat[0].className = currCat[0].className.replace(' act-cat', '');
-  el.className += ' act-cat';
+  let activeCategory = document.getElementsByClassName('active-category');
+  activeCategory[0].className = activeCategory[0].className.replace(' active-category', '');
+  el.className += ' active-category';
   let i = Array.from(el.parentNode.children).indexOf(el)
   categoryIndex = i
   createSubCategories(categoryIndex);
@@ -53,17 +67,20 @@ function catPos(el) {
   showCategoryDescription(categoryIndex);
 }
 
-function subPos(el) {
-  let current = document.getElementsByClassName('active');
-  current[0].className = current[0].className.replace(' active', '');
+/* Get the position of the current active Sub Category */
+function getSubcategoryPosition(el) {
+  let active = document.getElementsByClassName('active');
+  active[0].className = active[0].className.replace(' active', '');
   el.className += ' active';
   let i = Array.from(el.parentNode.children).indexOf(el)
   typeVal = i
   showSolutions(categoryIndex, typeVal);
 }
 
+/* Show the Solution Cards */
 function showSolutions(categoryIndex, typeVal) {
   var optionCards = document.getElementById('optionCards');
+  optionCards.classList.add('mt-4', 'row', 'row-cols-xl-3', 'row-cols-md-2', 'py-5');
   optionCards.innerHTML = '';
   var features = subCategories.features[categoryIndex].properties[typeVal].solutions;
   var category = subCategories.features[categoryIndex].category;
@@ -74,44 +91,36 @@ function showSolutions(categoryIndex, typeVal) {
     if (feature.lob == 'Enterprise') {
       feature.lob = '';
     } else if (feature.lob == 'Collegiate Hospitality') {
-      feature.lob = '<span class="ps-3"><img style="width: 25px; height: 25px;" src="assets/img/coho.svg"></span>';
+      feature.lob = '<span><img class="lob-icons" src="assets/img/coho.svg"></span>';
     } else if (feature.lob == 'Collegiate Hospitality,Workplace Experience') {
-      feature.lob = '<span class="ps-3"><img style="width: 25px; height: 25px;" src="assets/img/coho.svg"><img style="margin-left: 15px; width: 25px; height: 25px;" src="assets/img/wxg.svg"></span>';
+      feature.lob = '<span><img class="lob-icons" src="assets/img/coho.svg"><img class="lob-icons" src="assets/img/wxg.svg"></span>';
     } else if (feature.lob == 'Healthcare+,Collegiate Hospitality,Workplace Experience') {
-      feature.lob = '<span class="ps-3"><img style="width: 25px; height: 25px;" src="assets/img/coho.svg"><img style="margin-left: 15px; width: 25px; height: 25px;" src="assets/img/wxg.svg"><img style="margin-left: 15px; width: 25px; height: 25px;" src="assets/img/healthcare.svg"></span>';
+      feature.lob = '<span><img class="lob-icons" src="assets/img/coho.svg"><img class="lob-icons" src="assets/img/wxg.svg"><img class="lob-icons" src="assets/img/healthcare.svg"></span>';
     } 
-
     
     optionCards.innerHTML +=
-    '<div class="col mb-4">' +
-      '<div class="card h-100 card-bg p-2 d-flex flex-column">' +
-      '<div class="card-header border-0 bg-transparent py-0 d-flex justify-content-between">' +
-        '<div id="timeline-' + i + '">' + feature.timeline + '</div>' +
-        '<div id="costIcon-' + i + '">' + feature.costicon + '</div>' + 
-      '</div>' +
-      '<div class="card-body">' +
-        '<h6 class="card-title">' +
-          feature.name +
-        '</h6>' +
-        '<p class="text-trim one-'+ i + ' card-text">' + feature.description + '</p>' +
-      '</div>' +
-      feature.lob +
-      '<div class="card-footer  bg-transparent border-0 d-flex justify-content-between align-content-center">' +
-
-      '<a class="align-self-center" data-bs-toggle="modal" data-bs-target="#solutionsCard" href="javascript:void(0)"><h6 class="mb-0" style="font-size: 12px">Learn more</h6></a>' +
-      '<button onclick="return addRow(\'' + category + '\',\'' + feature.name +'\',\'' + feature.progression +'\',\'' + feature.costicon +'\',\'' + feature.timeline +'\')" class="btn btn-light btn-sm rounded-pill px-3 " type="button" data-bs-toggle="button" aria-pressed="true">Select</button>' +
-    '</div></div></div>'
+    `<div class="col mb-4">
+      <div class="card h-100 card-bg p-2 d-flex flex-column">
+        <div class="card-body">
+          <h6 class="card-title">${feature.name}</h6>
+          <p class="mt-3 text-trim one-${i} card-text">${feature.description}</p>
+        </div>
+        <div class="card-footer bg-transparent border-0 d-flex flex-row-reverse align-items-center justify-content-between">
+          <button onclick="return addRow(${category}, ${feature.name}, ${feature.progression}, ${feature.costicon}, ${feature.timeline})" class="btn btn-light btn-sm rounded-pill px-3" type="button" data-bs-toggle="button" aria-pressed="true">Select</button>
+          ${feature.lob}
+        </div>
+      </div>
+    </div>`
     i++;
   }); 
 
+  /* Add elipsis for trimmed text, i.e.: "Read More" */
   for (let i = 0; i < features.length; i++) {
-    var timeline = document.getElementById('timeline-' + i);
-    var costIcon = document.getElementById('costIcon-' + i);
     $(function () {
       function trimText(selector, limit) {    
         var text = selector.text(),
           trim;
-    
+
         selector.each(function() {
           if ($(this).text().length > limit) {
             trim = $(this).text().substr(0, limit);
@@ -130,42 +139,5 @@ function showSolutions(categoryIndex, typeVal) {
       };
       trimText($(".one-" + i),   60);
     });
-    if (timeline.innerHTML == 'Quarter') {
-      timeline.innerHTML = '<img src="assets/img/timeline.svg" alt="timeline"><span style="font-size: 12px;"> 0-3 months</span>'
-    } else if(timeline.innerHTML == 'Half') {
-      timeline.innerHTML = '<img src="assets/img/timeline.svg" alt="timeline"><span style="font-size: 12px;"> 4-6 months</span>'
-    } else if(timeline.innerHTML == 'Three Quarter') {
-      timeline.innerHTML = '<img src="assets/img/timeline.svg" alt="timeline"><span style="font-size: 12px;"> 7-9 months</span>'
-    } else if(timeline.innerHTML == 'Full') {
-      timeline.innerHTML = '<img src="assets/img/timeline.svg" alt="timeline"><span style="font-size: 12px;"> 10-12+ months</span>'
-    } else if(timeline.innerHTML == 'Empty') {
-      timeline.innerHTML = '<img src="assets/img/timeline.svg" alt="timeline"><span style="font-size: 12px;"> Immediate</span>'
-    } else { timeline.innerHTML = ''; }
-    if (costIcon.innerHTML == 'One') {
-      costIcon.innerHTML = '<span style="color: #007B8A; font-family: gotham-medium;">$<span style="color: rgba(0, 123, 138, 0.40);">$$$$</span></span>'
-    } else if (costIcon.innerHTML == 'Two') {
-      costIcon.innerHTML = '<span style="color: #007B8A; font-family: gotham-medium;">$$<span style="color: rgba(0, 123, 138, 0.40);">$$$</span></span>' 
-    } else if (costIcon.innerHTML == 'Three') {
-      costIcon.innerHTML = '<span style="color: #007B8A; font-family: gotham-medium;">$$$<span style="color: rgba(0, 123, 138, 0.40);">$$</span></span>' 
-    } else if (costIcon.innerHTML == 'Four') {
-      costIcon.innerHTML = '<span style="color: #007B8A; font-family: gotham-medium;">$$$$<span style="color: rgba(0, 123, 138, 0.40);">$</span></span>' 
-    } else if (costIcon.innerHTML == 'Five') {
-      costIcon.innerHTML = '<span style="color: #007B8A; font-family: gotham-medium;">$$$$$</span>' 
-    } else if (costIcon.innerHTML = 'Empty') {
-      costIcon.innerHTML = '';
-    } else { costIcon.innerHTML = ''; }
   }
 }
-
-function showCategoryDescription(categoryIndex) {
-  var categoryDescription = document.getElementById('categoryDescription');
-  categoryDescription.innerHTML = '';
-  var feature = subCategories.features[categoryIndex];
-  categoryDescription.innerHTML += 
-  '<h1 class="text-light fw-bold">' + feature.category + '</h1>' +
-  '<hr class="text-bg-dark w-50 p-0">' +
-  '<h6 class="text-light">' + feature.subheader + '</h6>' +
-  '<p class="py-2 text-light fs-6">' + feature.description + '</p>'
-}
-
-
