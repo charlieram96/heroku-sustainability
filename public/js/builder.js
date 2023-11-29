3d446698d51c8bc466a05445a1eb2a54b814bc86
dylan-mdg <dylan@maxwelldesigngroup.com>
2023-11-29 12:38:20 -0500

    updates

--- public/js/builder.js
+++ public/js/builder.js
@@ -82,14 +82,14 @@ function getSubcategoryPosition(el) {
 /* Show the Solution Cards */
 function showSolutions(categoryIndex, typeVal) {
   var optionCards = document.getElementById('optionCards');
-  optionCards.classList.add('mt-4', 'row', 'row-cols-xl-3', 'row-cols-md-2', 'py-5');
+  optionCards.classList.add('mt-4', 'row', 'row-cols-xl-3', 'row-cols-md-2', 'pb-5');
   optionCards.innerHTML = '';
   var features = subCategories.features[categoryIndex].properties[typeVal].solutions;
 
-  // Not needed
-  var category = subCategories.features[categoryIndex].category;
-  let i = 0;
-  // 
+
+
+
+
 
   let selectedCosts = Array.from(document.querySelectorAll('.cost-filter:checked')).map(el => el.value);
   let selectedTimelines = Array.from(document.querySelectorAll('.timeline-filter:checked')).map(el => el.value);
@@ -118,12 +118,12 @@ function showSolutions(categoryIndex, typeVal) {
           <p class="mt-3 text-trim one-${i} card-text">${feature.description}</p>
         </div>
         <div class="card-footer bg-transparent border-0 d-flex flex-row-reverse align-items-center justify-content-between">
-          <button onclick="return addRow('${subCategories.features[categoryIndex].category}', '${feature.name}', '${feature.progression}', '${feature.costicon}', '${feature.timeline}')" class="btn btn-light btn-sm rounded-pill px-3" type="button" data-bs-toggle="button" aria-pressed="true">Select</button>
+        <button onclick="addActive('${feature.name}', '${feature.id}'); return addRow('${subCategories.features[categoryIndex].category}', '${feature.name}', '${feature.progression}', '${feature.costicon}', '${feature.timeline}','${feature.id}', '${feature.commitment}')" class="btn btn-light btn-sm rounded-pill px-3" type="button" data-bs-toggle="button" aria-pressed="true" id="active-check-${feature.id}">Select</button>
           ${feature.lob}
         </div>
       </div>
     </div>`;
-
+ 
     if (feature.active === ' active') {
       var activeLook = document.getElementById('active-check-' + feature.id);
       activeLook.className += ' active';
